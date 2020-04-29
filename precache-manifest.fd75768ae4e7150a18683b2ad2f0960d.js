e2b11bd55038177141f2b31c923d3a87353596b0
nomy2333 <yuanxilan2333@gmail.com>
2020-04-29 22:28:44 +1000

    Updates

---
+++ precache-manifest.fd75768ae4e7150a18683b2ad2f0960d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca9c4731d840ebb19962c7867a9a30ad",
+    "url": "/monsters-app/index.html"
+  },
+  {
+    "revision": "6f3cf31a40bbf40d8633",
+    "url": "/monsters-app/static/css/main.d02a80b0.chunk.css"
+  },
+  {
+    "revision": "8ad4a3ce4d413a7e8488",
+    "url": "/monsters-app/static/js/2.9c35b6e2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-app/static/js/2.9c35b6e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f3cf31a40bbf40d8633",
+    "url": "/monsters-app/static/js/main.9c41e064.chunk.js"
+  },
+  {
+    "revision": "5200753bc18587e6a08e",
+    "url": "/monsters-app/static/js/runtime-main.3d3a089a.js"
+  }
+]);
